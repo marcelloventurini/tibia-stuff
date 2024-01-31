b96fd95e8792d9cdb001a7e3ff6e57e579a51b7a
Marcello Venturini <marcellofventurini@gmail.com>
2024-01-31 16:14:13 -0300

    feat: create a function to show the boosted creature

--- src/partials/Boosted/Boosted.tsx
+++ src/partials/Boosted/Boosted.tsx
@@ -1,14 +1,22 @@
 import { useEffect, useState } from 'react'
 import styles from './Boosted.module.scss'
 
-interface Boosted {
+interface BoostedBoss {
   featured: boolean
   image_url: string
   name: string
 }
 
+interface BoostedCreature {
+  name: string
+  race: string
+  image_url: string
+  featured: boolean
+}
+
 export default function Boosted() {
-  const [boostedBoss, setBoostedBoss] = useState<Boosted>()
+  const [boostedBoss, setBoostedBoss] = useState<BoostedBoss>()
+  const [boostedCreature, setBoostedCreature] = useState<BoostedCreature>()
 
   async function getBoostedBoss() {
     const data = await fetch('https://api.tibiadata.com/v4/boostablebosses')
@@ -17,16 +25,32 @@ export default function Boosted() {
     setBoostedBoss(boosted.boostable_bosses.boosted)
   }
 
+  async function getBoostedCreature() {
+    const data = await fetch('https://api.tibiadata.com/v4/creatures')
+    const boosted = await data.json()
+
+    setBoostedCreature(boosted.creatures.boosted)
+  }
+
   useEffect(() => {
     getBoostedBoss()
+  })
+
+  useEffect(() => {
+    getBoostedCreature()
   })
 
   return (
     <section className={styles.boosted}>
       <h2>Boosted</h2>
-      <div>
-        {boostedBoss?.name}
+      <div className={styles.boostedBoss}>
+        <h3>{boostedBoss?.name}</h3>
         <img src={boostedBoss?.image_url} alt={boostedBoss?.name} />
+      </div>
+
+      <div className={styles.boostedCreature}>
+        <h3>{boostedCreature?.name}</h3>
+        <img src={boostedCreature?.image_url} alt={boostedCreature?.name} />
       </div>
     </section>
   )
