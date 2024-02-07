401bbdd7efcec538cbcf43b5afcf2b1083b7ae3e
Marcello Venturini <marcellofventurini@gmail.com>
2024-02-07 11:05:49 -0300

    refactor: move the api call logic back to the boosted section

--- src/components/Boosted/Boosted.tsx
+++ src/components/Boosted/Boosted.tsx
@@ -1,51 +1,16 @@
-import { useEffect, useState } from 'react'
 import styles from './Boosted.module.scss'
 
-interface BoostedBoss {
-  featured: boolean
-  image_url: string
-  name: string
-}
+// interface BoostedProps {
+//   image:
+// }
 
-interface BoostedCreature {
-  name: string
-  race: string
-  image_url: string
-  featured: boolean
-}
-
-export default function Boosted() {
-  const [boostedBoss, setBoostedBoss] = useState<BoostedBoss>()
-  const [boostedCreature, setBoostedCreature] = useState<BoostedCreature>()
-
-  async function getBoostedBoss() {
-    const data = await fetch('https://api.tibiadata.com/v4/boostablebosses')
-    const boosted = await data.json()
-
-    setBoostedBoss(boosted.boostable_bosses.boosted)
-  }
-
-  async function getBoostedCreature() {
-    const data = await fetch('https://api.tibiadata.com/v4/creatures')
-    const boosted = await data.json()
-
-    setBoostedCreature(boosted.creatures.boosted)
-  }
-
-  useEffect(() => {
-    getBoostedBoss()
-  })
-
-  useEffect(() => {
-    getBoostedCreature()
-  })
-
+export default function Boosted({ image, name }) {
   return (
     <div className={styles.boostedBoss}>
       <h3 className={styles.subtitle}>Boss do Dia</h3>
       <div className={styles.container}>
-        <img src={boostedBoss?.image_url} alt={boostedBoss?.name} />
-        <h4>{boostedBoss?.name}</h4>
+        <img src={image} alt={name} />
+        <h4>{name}</h4>
       </div>
     </div>
   )
--- src/partials/BoostedSection/BoostedSection.tsx
+++ src/partials/BoostedSection/BoostedSection.tsx
@@ -1,10 +1,49 @@
+import { useEffect, useState } from 'react'
 import Boosted from '../../components/Boosted/Boosted'
 import styles from './BoostedSection.module.scss'
 
+interface BoostedBoss {
+  featured: boolean
+  image_url: string
+  name: string
+}
+
+interface BoostedCreature {
+  name: string
+  race: string
+  image_url: string
+  featured: boolean
+}
+
 export default function BoostedSection() {
+  const [boostedBoss, setBoostedBoss] = useState<BoostedBoss>()
+  const [boostedCreature, setBoostedCreature] = useState<BoostedCreature>()
+
+  async function getBoostedBoss() {
+    const data = await fetch('https://api.tibiadata.com/v4/boostablebosses')
+    const boosted = await data.json()
+
+    setBoostedBoss(boosted.boostable_bosses.boosted)
+  }
+
+  async function getBoostedCreature() {
+    const data = await fetch('https://api.tibiadata.com/v4/creatures')
+    const boosted = await data.json()
+
+    setBoostedCreature(boosted.creatures.boosted)
+  }
+
+  useEffect(() => {
+    getBoostedBoss()
+  })
+
+  useEffect(() => {
+    getBoostedCreature()
+  })
+
   return (
     <section className={styles.boosted}>
-      <Boosted />
+      <Boosted image={boostedBoss?.image_url} name={boostedBoss?.name} />
     </section>
   )
 }
