461a3af6c588837b20160f4953b8d189b0c0ad3d
Marcello Venturini <marcellofventurini@gmail.com>
2024-02-08 16:28:01 -0300

    feat: remove news section; boosted section updated

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,12 +1,10 @@
 import BoostedSection from '../../partials/BoostedSection/BoostedSection'
-import NewsPreview from '../../partials/NewsPreview/NewsPreview'
 import styles from './Home.module.scss'
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <BoostedSection />
-      <NewsPreview />
     </div>
   )
 }
