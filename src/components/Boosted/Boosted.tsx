9b1bc9f4fcdbb1a62b0a65ab17f7c9338996e463
Marcello Venturini <marcellofventurini@gmail.com>
2024-02-11 13:28:21 -0300

    feat: restyle the boost section; now the creatures are aligned in the center

--- src/components/Boosted/Boosted.tsx
+++ src/components/Boosted/Boosted.tsx
@@ -8,12 +8,12 @@ interface BoostedProps {
 
 export default function Boosted({ image, name, type }: BoostedProps) {
   return (
-    <div>
+    <div className={styles.boosted}>
       <h3 className={styles.subtitle}>{type} do dia</h3>
       <div className={styles.container}>
-        <img src={image} alt={name} />
-        <h4>{name}</h4>
+        <img src={image} alt={name} className={styles.img} />
       </div>
+      <h4>{name}</h4>
     </div>
   )
 }
