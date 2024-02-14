813e2c98c175b95f50e62c298a5a11453f69154e
Marcello Venturini <marcellofventurini@gmail.com>
2024-02-14 16:55:16 -0300

    feat: set a size for main; add a separator line

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -5,7 +5,8 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <main>
-        <h1>Guia de Vocações</h1>
+        <h1 className={styles.title}>Guia de Vocações</h1>
+        <hr />
       </main>
       <BoostedSection />
     </div>
